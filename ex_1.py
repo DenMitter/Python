aff797bcffaeadc5297c355e0340a8ad694dd2a9
unknown <57758gfk@gmail.com>
2022-11-03 21:23:26 +0300

    Add HW

---
+++ ex_1.py
@@ -0,0 +1,7 @@
+int_one = int(input("\nВведіть перше число: "))
+int_two = int(input("Введіть друге число: "))
+int_three = int(input("Введіть третє число: "))
+resault_plus = int_one+int_two+int_three
+resault_dob = int_one*int_two*int_three
+ 
+print('\nСума чисел:', resault_plus, '\nДобуток чисел:', resault_dob)
