0a775943e53077267491441971baa5a2ebc233ba
unknown <57758gfk@gmail.com>
2022-11-03 21:05:40 +0300

    Rem HW ex_1 and add WH ex_2

--- ex_2.py
+++ ex_2.py
@@ -1,5 +1,7 @@
 int_one = int(input('Введіть перше число: '))
 int_two = int(input('Введіть друге число: '))
 int_three = int(input('Введіть третє число: '))
+int_fore = int(input('Введіть четверте число: '))
+resault = int_one*int_two*int_three*int_fore
 
-print(f'Результат: {int_one}{int_two}{int_three}')
+print(f'Результат: {resault}')
