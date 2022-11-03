4abb81054879e4e1e310ad5dc1727ad1f5653c0f
unknown <57758gfk@gmail.com>
2022-11-03 21:01:31 +0300

    Rem HW ex_2 and add WH ex_3

--- ex_2.py
+++
@@ -1,6 +0,0 @@
-int_one = int(input("\nВведіть дохід за місяць: "))
-int_two = int(input("Введіть витрати по кридиту за місяць: "))
-int_three = int(input("Введіть витрати на комунальні послуги: "))
-resault = int_one-int_two-int_three
-
-print('\nВаш залишок:', resault)
