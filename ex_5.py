061208cec760a46d659d0ba962d0defd7947c2b7
DenMitter <104051796+DenMitter@users.noreply.github.com>
2022-11-08 16:42:20 +0200

    Update ex_5.py

--- ex_5.py
+++ ex_5.py
@@ -1,10 +1,9 @@
 num_one = int(input('Введіть перше число: '))
 num_two = int(input('Введіть друге число: '))
-action = int(input('Що робимо ( множити 1 | відняти 2 | додати 3): '))
+action = int(input('Що робимо ( множити 1 | відняти 2 | додати 3 | avg 4): '))
 
 if action == 1: print(f'Результат: {num_one * num_two}')
 elif action == 2: print(f'Результат: {num_one - num_two}')
 elif action == 3: print(f'Результат: {num_one + num_two}')
-else:
-    if num_one == num_two: print(f'Результат: {num_one} = {num_two}')
-    else: print(f'Результат: числа не рівні, виберіть іншу дію або введіть другі числа')
+elif action == 4: print(f'Результат: {num_one + num_two / 2}')
+else: print('Command not found')
