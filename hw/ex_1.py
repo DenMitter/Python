e547c0d2690d093336cbbc9df23433729101f142
Студент <student@ITStep.local>
2022-11-08 16:22:51 +0200

    Rem hw in master

--- hw/ex_1.py
+++
@@ -1,7 +0,0 @@
-num_one = int(input('Введіть перше число: '))
-num_two = int(input('Введіть друге число: '))
-num_three = int(input('Введіть третє число: '))
-action = int(input('Що робимо ( множити 1 | додати 2 ): '))
-
-if action == 1: print(f'Результат: {num_one * num_two * num_three}')
-else: print(f'Результат: {num_one + num_two + num_three}')
