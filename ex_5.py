4b4e5d06ea254991c58533327e9c419e33c1a2c2
unknown <57758gfk@gmail.com>
2022-12-06 18:05:59 +0300

    Add lab

--- ex_5.py
+++ ex_5.py
@@ -1,8 +1,9 @@
 try:
     begin = int(input('begin->'))
     end = int(input('end->'))
-    for item in range(end, begin-1, -1):
-        print(item, end='\t')
+
+    if begin > end: end, begin = begin, end
+    for item in range(begin, end+1): print(item, end='\t')
 
 except ValueError as vl_ex:
     print(f'Value error: {vl_ex}')
