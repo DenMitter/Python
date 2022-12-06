2287c738d6384be46fd483086e28d0ba4495781e
unknown <57758gfk@gmail.com>
2022-12-06 18:05:13 +0300

    Edit lab

--- ex_4.py
+++ ex_4.py
@@ -1,4 +1,11 @@
-begin = int(input('begin->'))
-end = int(input('end->'))
-for item in range(end, begin-1, -1):
-    print(item, end='\t')
+try:
+    begin = int(input('begin->'))
+    end = int(input('end->'))
+    for item in range(end, begin-1, -1):
+        print(item, end='\t')
+
+except ValueError as vl_ex:
+    print(f'Value error: {vl_ex}')
+except Exception as ex:
+    print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
