a7522b98604323ec5a2944a111909f96388825b2
unknown <57758gfk@gmail.com>
2022-12-14 10:54:36 +0300

    Add hw

---
+++ ex_2.py
@@ -0,0 +1,12 @@
+try:
+    length = int(input('length->'))
+    symbol = input('symbol->')
+
+    for item in range(0, length):
+        print(symbol, end='\n')
+
+except ValueError as vl_ex:
+    print(f'Value error: {vl_ex}')
+except Exception as ex:
+    print(f'Error: {ex}')
+    print(f'Name: {ex.__class__.__name__}')
