7d105f9c624b82891846031e263c2cbc6246ae8f
unknown <57758gfk@gmail.com>
2022-12-20 17:58:56 +0300

    Edit lab

--- ex_3.py
+++ ex_3.py
@@ -1,15 +1,19 @@
 try:
     start = int(input('\nВведіть початок діапазону: '))
     end = int(input('\nВведіть цінець діапазону: '))
-    num = int(input('\nВведіть число: '))
 
-    if num > end or num < start:
-        while num > start and num < end:  
-            num = int(input('\nВведіть число яке потрапляє в діапазон: '))
+    if start > end: 
+        start, end = end, start
 
-    for i in range(start, end):
-        print(i)
-        if i == num: print(f'!{num}!')
+    while True:
+        num = int(input('\nВведіть число: '))
+        if start <= num and num <= end:
+            for i in range(start, end+1):
+                if i < num or i > num: print(i, end=" ")
+                else: print(f'!{i}!', end=" ")
+            break
+        else: 
+            print('Число не входить у діапазон')
 
 except Exception as ex:
     print(f'Error [{ex.__class__.__name__}]: {ex}')
