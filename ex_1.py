d3fb717816220732f0ff8b891f54a0c197843b3a
unknown <57758gfk@gmail.com>
2022-12-20 17:40:14 +0300

    Add hw

--- ex_1.py
+++ ex_1.py
@@ -1,6 +1,8 @@
 try:
-    num = int(input('Введіть число: '))
-    for i in range(1, 10):
-        print(f'Результат: {num*i}')
+    num_one = int(input('Введи перше число: '))
+    num_two = int(input('Введи друге число: '))
+
+    print(f'Результат: {num_one**num_two}')
+
 except Exception as ex:
     print(f'Error [{ex.__class__.__name__}]: {ex}')
