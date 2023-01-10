f9f1acb9d7cf9127cedbea3982c26c2be35c81d4
unknown <57758gfk@gmail.com>
2023-01-10 18:30:20 +0300

    Add lab

---
+++ ex_4.py
@@ -0,0 +1,17 @@
+try:
+    num_one = int(input("Введіть ширину квадрата: "));
+    num_two = int(input("Введіть висоту квадрата: "));
+
+    for i in range(0, num_one):
+        for x in range(0, num_two):
+            if i == 0 or i == num_one-1: 
+                print("*", end=' ')
+            else: 
+                if x == 0 or x == num_two-1: 
+                    print("*", end=' ')
+                else:
+                    print(" ", end=' ')
+        print()
+
+except Exception as ex:
+    print(f'Error [{ex.__class__.__name__}]: {ex}')
