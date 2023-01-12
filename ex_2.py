f1b3188d34c02530ea09d95896c1bfd5c2f57bf0
unknown <57758gfk@gmail.com>
2023-01-12 16:41:47 +0300

    Add hw

--- ex_2.py
+++ ex_2.py
@@ -1,17 +1,14 @@
 try:
-    num_one = int(input("Введіть початок: "));
-    num_two = int(input("Введіть кінець: "));
+    # num = int(input("Введіть число: "));
 
-    if num_one > num_two:
-        num_one, num_two = num_two, num_one
+    for i in range(1, 10+1):
+        for x in range(1, 10+1):
+            print(f'{i} * {x} = {i*x}', end='\t\t')
+        print()
 
-    for i in range(num_one, num_two+1):
-        counter = 0
-        for x in range(1, i+1):
-            if i % x == 0:
-                counter += 1
-        if counter == 2:
-            print(i, end=' ')
+        for x in range(1, 235):
+            print('-', end='')
+        print()
     print()
 
 except Exception as ex:
