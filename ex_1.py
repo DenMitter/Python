337bb3a5d5bd9e85fcd70b3690ec8822493aac7c
unknown <57758gfk@gmail.com>
2023-01-12 16:35:47 +0300

    Edit hw

--- ex_1.py
+++ ex_1.py
@@ -2,8 +2,8 @@ try:
     num_one = int(input("Введіть початок: "));
     num_two = int(input("Введіть кінець: "));
 
-    # if num_one > num_two:
-    #     num_one, num_two = num_two, num_one
+    if num_one > num_two:
+        num_one, num_two = num_two, num_one
 
     for i in range(num_one, num_two+1):
         counter = 0
