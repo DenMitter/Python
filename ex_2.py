28585451633100b12298165c18656aa1db975b16
unknown <57758gfk@gmail.com>
2023-01-21 16:49:02 +0200

    Add lab

--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,16 @@
 try:
-    text = input("Введіть текст: ")
-    find = input("Введіть символ для пошуку: ")
-    len_text = len(text)
-    print(f'Кількість літер: {len_text}\nКількість найдених символів: {text.find(find, 0)}')
+    string = input('Введіть текст: ')
+    letter_count = 0
+    digit_count = 0
+
+    for char in string:
+        if char.isalpha():
+            letter_count += 1
+        elif char.isdigit():
+            digit_count += 1
+
+    print('Кількість літер:', letter_count)
+    print('Кількість цифр:', digit_count)
 
 except Exception as ex:
     print(f'Error [{ex.__class__.__name__}]: {ex}')
