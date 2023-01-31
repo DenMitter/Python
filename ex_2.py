f0c3bca8450f73c1a77032016b4b6403f67c8ec1
unknown <57758gfk@gmail.com>
2023-01-31 12:51:21 +0200

    Add lab

--- ex_2.py
+++ ex_2.py
@@ -4,19 +4,20 @@ try:
     lengthOflist = int(input('Length of list->'))
     begin = int(input('begin->'))
     end = int(input('end->'))
+    zero, positive, negative = 0, 0, 0
+
     if begin > end:
         begin, end = end, begin
     numbers = list()
     for i in range(0, lengthOflist):
         numbers.append(random.randint(begin, end))
+        if numbers[i] == 0: zero += 1
+        elif numbers[i] > 0: positive += 1
+        elif numbers[i] < 0: negative += 1
     print(numbers)
 
-    partList = numbers[numbers.index(max(numbers))+1: numbers.index(min(numbers))].copy()
-    print(partList)
-    mul = 1
-    for item in partList:
-        mul *= item
-    print(mul)
+    print(f'max-> {max(numbers)}\nmin-> {min(numbers)}')
+    print ("\nzero->", zero, "\npositive->", positive, "\nnegative->", negative)
     
 
 except Exception as ex:
