4a040c07e7e4a748718ae2c5f21c012146fc0814
unknown <57758gfk@gmail.com>
2023-02-02 10:14:26 +0200

    Add hw

--- ex_1.py
+++ ex_1.py
@@ -1,36 +1,21 @@
-import random
 try:
-    length = int(input("Введіть кількість елементів у списках: "))
-    choice = int(input("1) Елементи обох списків \n2) Елементи обох списків без повторень \n3) Елементи, спільні для двох списків \n4) Тільки унікальні елементи кожного зі списків \n5) Усе вище \n Введіть свій вибір:"))
-    list1 = []
-    list2 = []
+    operation = input("operation-> ")
+    parts = operation.split()
+    operation_one = 0
 
-    if length > 0:
-        for i in range(length):
-            list1.append(random.randint(0, 100))
-            list2.append(random.randint(0, 100))
+    if parts[1] == "+":
+         operation_one = int(parts[0]) + int(parts[2])
 
-        print("Список 1: ", list1)
-        print("Список 2: ", list2)
+    elif parts[1] == "*":
+        operation_one = int(parts[0]) * int(parts[2])
 
-        if choice == 1 or choice == 5:
-            list3 = list1 + list2
-            print("Елементи обох списків: ", list3)
+    elif parts[1] == "-":
+         operation_one = int(parts[0]) - int(parts[2])
 
-        if choice == 2 or choice == 5:
-            list3 = list(set(list1 + list2))
-            print("Елементи обох списків без повторів: ", list3)
+    elif parts[1] == "/":
+         operation_one = round(int(parts[0]) / int(parts[2]))
 
-        if choice == 3 or choice == 5:
-            list3 = list(set(list1) & set(list2))
-            print("Елементи, спільні для двох списків: ", list3)
-
-        if choice == 4 or choice == 5:
-            list3 = list(set(list1) ^ set(list2))
-            print("Тільки унікальні елементи кожного зі списків: ", list3)
-
-    else:
-        print("Елементи в списках мають бути додатними")
+    print(operation_one)
     
 
 except Exception as ex:
